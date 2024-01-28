9041bd466a1dc6aae52b1a04305a7f65443b0df1
Martin Algesten <martin@algesten.se>
2024-01-28 13:10:53 +0100

    Clean slate

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,1 @@
-//! str0m DTLS implementation.
-//!
-//! A [DTLS 1.2][dtls] implementation, which in turns is a variant
-//! of [TLS 1.2][tls].
-//!
-//! [dtls]: https://datatracker.ietf.org/doc/html/rfc6347
-//! [tls] : https://datatracker.ietf.org/doc/html/rfc5246
 
-mod error;
-pub use error::Error;
-
-mod codec;
-
-pub(crate) mod types;
-
-mod record;
-pub use record::DtlsPlainText;
